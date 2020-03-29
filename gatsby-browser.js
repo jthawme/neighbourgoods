c46304e89503840223716f82bd1a9b78bb6444d5
Jonathan Thaw <hi@jthaw.me>
2020-03-29 11:43:17 +0100

    small things

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,8 @@
+import React from "react";
+
 import wrapWithProvider from "./reduxProvider";
 export const wrapRootElement = wrapWithProvider;
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents(<h1 id="main-logo">Neighbourgoods</h1>);
+};
